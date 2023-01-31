22a479bb292794d0fecdbd471a2fbc3ecc3e96f3
Connor Henderson <connorhenderson20@gmail.com>
2023-01-30 23:28:22 -0500

    add basic unit test

--- test_functions.py
+++ test_functions.py
@@ -1,2 +1,15 @@
 import pytest
+from testfixtures import TempDirectory
 
+import file_functions
+
+
+def test_print_file(capfd):
+    with TempDirectory() as tempdir:
+        temp_file = 'test.text'
+        test_line = b'Hello'
+        tempdir.write(temp_file, test_line)
+        file_path = tempdir.path + '\\' + temp_file
+        file_functions.print_file(file_path)
+    out, err = capfd.readouterr()
+    assert out == 'HELLO\n'
