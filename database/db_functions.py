de1f8fff8a4f156595033e83b7d5e2e48d30ba93
Connor Henderson <connorhenderson20@gmail.com>
2023-03-10 13:19:48 -0500

    fix pipeline

--- database/db_functions.py
+++ database/db_functions.py
@@ -74,16 +74,16 @@ def insert_db(database, table, data):
                                item.get('Field12', ''),
                                # Time period
                                ' '.join([item.get('Field213', ''),
-                                          item.get('Field214', ''),
-                                          item.get('Field215', ''),
-                                          item.get('Field216', ''),
-                                          item.get('Field217', '')]),
+                                         item.get('Field214', ''),
+                                         item.get('Field215', ''),
+                                         item.get('Field216', ''),
+                                         item.get('Field217', '')]),
                                # perms
                                item.get('Field313', ''),
                                # opportunities
                                ' '.join([item.get('Field113', ''), item.get('Field114', ''), item.get('Field115', ''),
-                                          item.get('Field116', ''), item.get('Field117', ''), item.get('Field118', ''),
-                                          item.get('Field119', '')]),
+                                         item.get('Field116', ''), item.get('Field117', ''), item.get('Field118', ''),
+                                         item.get('Field119', '')]),
                                # created
                                item.get('DateCreated', ''),
                                # created by
